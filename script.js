fbbc20793276baa410eecd387ea7c6cd10a0c2bb
Pournami-P <pournamip2005@gmail.com>
2026-01-04 19:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let errorMsg=document.getElementById("error")
+
+function clickIt(){
+    errorMsg.innerText="Something went wrong"
+
+    /*setTimeout(() => {
+        errorMsg.style.opacity = "0";
+    }, 2000);*/
+}
